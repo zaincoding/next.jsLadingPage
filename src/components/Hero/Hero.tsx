221c447ae040599011f37ab91ca4d8efeb61bf82
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-10-14 23:51:50 +0500

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,9 +8,7 @@ const Hero = () =>{
     <h1 className={hero.h1}>Welcom To My Awosem Website</h1>
     <p className={hero.p}>Discover amazing features and services to boost your<br/><span className={hero.span}> online presence.
         <br/> <button className={hero.btn}>Get Started</button></span></p>
-       <img src="../../../images/herobg.PNG" className={hero.img}></img>
-       <img src="../../../images/logo2.PNG"></img>
-
+       <Image src="../../../images/herobg.PNG" className={hero.img}>
 
    </div>
   )
