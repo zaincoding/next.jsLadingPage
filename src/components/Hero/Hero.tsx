7c7f9ea6f5a5d1b0db5f96c10c695a91336bec31
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-10-15 09:23:11 +0500

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import hero from './hero.module.css'
 console.log(hero, "CSS-Hero")
 
@@ -8,7 +7,7 @@ const Hero = () =>{
     <h1 className={hero.h1}>Welcom To My Awosem Website</h1>
     <p className={hero.p}>Discover amazing features and services to boost your<br/><span className={hero.span}> online presence.
         <br/> <button className={hero.btn}>Get Started</button></span></p>
-       <Image src="/images/herobg.PNG"  alt="Hero background"className={hero.img}/>
+       <img src="../../../images/herobg.PNG"  alt="Hero background"className={hero.img}></img>
 
    </div>
   )
